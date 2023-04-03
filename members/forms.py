47f67bb3be097ea19f47ec466c8bbe7255e63191
Omnistudent <theostenman@yahoo.com>
2023-04-03 06:22:25 +0200

    'simplified_register'

--- members/forms.py
+++ members/forms.py
@@ -4,14 +4,15 @@ from django import forms
 
 
 class RegisterUserForm(UserCreationForm):
-	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-	first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-	last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+	#email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+	#first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+	#last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
 	
 
 	class Meta:
 		model = User
-		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+		#fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+		fields = ('username', 'password1', 'password2')
 
 
 	def __init__(self, *args, **kwargs):
